429fcae7600e017e90cc8ad04cd79760d5a7bc7a
wupa9 <wupaz9@gmail.com>
2017-02-03 03:37:46 -0600

    Move constants to own file

---
+++ app/constants.js
@@ -0,0 +1,6 @@
+export const defaultShape = 'ellipse'
+export const initShape    = 'diamond'
+export const finalColor   = '#F44242'
+export const defaultColor = '#97C2FC'
+
+export const stateFormUrl = './app/modals/state.html'
