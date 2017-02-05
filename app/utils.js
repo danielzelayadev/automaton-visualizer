e9e65d6b704e958f08f3b941e1559bf7d29601ec
wupa9 <wupaz9@gmail.com>
2017-02-05 03:05:17 -0600

    Remove run automaton

--- app/utils.js
+++ app/utils.js
@@ -1,12 +1,3 @@
 export function getSetFromString (str) {
     return str.split('').filter((c, i, a) => i === a.indexOf(c))
 }
-
-export function runAutomaton (automaton) {
-    if (!automaton) return
-    try {
-        alert(automaton.run($('[name="testStr"]').val()) ? "Valid String!" : "Invalid String!")
-    } catch (e) {
-        alert(e.message)
-    }
-}
