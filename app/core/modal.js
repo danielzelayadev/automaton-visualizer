98ef4491a36a8b1365404f5b0d476a3e9e6bc1e1
wupa9 <wupaz9@gmail.com>
2017-02-05 04:37:02 -0600

    Focus first modal form input and close on enter press

--- app/core/modal.js
+++ app/core/modal.js
@@ -8,11 +8,19 @@ export default class Modal {
     loadForm(url = '', form = { name: '', vals: {} }, onSubmit = v => {}, onCancel = () => {}) {
         this.mdl.load(url, () => {
             form = new Form(form.name, form.vals)
-            
+
             this.mdl.modal({ dismissible: false })
             this.mdl.modal('open')
+            this.mdl.off('keypress')
+            this.mdl.keypress(e => this.closeOnEnter(e.which, '#submit'))
             this.mdl.find('#submit').click(e => onSubmit(form.getValues()))
             this.mdl.find('#cancel').click(e => onCancel())
+
+            form.inputs[0].focus()
         })
+    }
+    closeOnEnter (key, closeId) {
+        if (key === 13) 
+            this.mdl.find(closeId).click()
     }
 }
