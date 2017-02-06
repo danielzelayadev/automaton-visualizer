62e6379fe1274d7b353afdd0a3a82ebc864d292e
wupa9 <wupaz9@gmail.com>
2017-02-05 23:17:47 -0600

    Run test string on enter press

--- app/index.js
+++ app/index.js
@@ -7,6 +7,7 @@ const edges = new vis.DataSet([])
 
 const select   = $('select')
 const resetBtn = $('#reset-btn')
+const testStr  = $('input[name="testStr"]')
 
 const alphabetPromptText = 'Please enter alphabet string: '
 
@@ -15,6 +16,7 @@ let currentVisualizer = 'DFA'
 select.material_select()
 select.change(onVisualizerChange)
 resetBtn.click(e => start(prompt(alphabetPromptText)))
+testStr.keypress(e => clickRunBtn(e.which))
 
 start('01')
 
@@ -53,4 +55,9 @@ function onVisualizerChange() {
         currentVisualizer = visSelection
         start(alphabetStr)
     }
+}
+
+function clickRunBtn(key) {
+    if (key === 13)
+        $('#run-btn').click()
 }
