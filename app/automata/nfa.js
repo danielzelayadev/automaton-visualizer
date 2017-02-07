3ab992996389233c3defa1b44266c6b3e1bc0eb9
wupa9 <wupaz9@gmail.com>
2017-02-06 20:07:36 -0600

    Extra transition validations and new methods to reuse parent code

--- app/automata/nfa.js
+++ app/automata/nfa.js
@@ -5,25 +5,6 @@ import { UnknownCharError, UnknownStateError,
 		 DuplicateStateError, DuplicateTransitionError } from '../errors'
 
 export default class NFA extends Automaton {
-	addTransition (from, a, to) {
-		const fs = this.getState(from)
-		const ts = this.getState(to)
-
-		if (!fs)
-			throw new UnknownStateError(from)
-		if (!ts)
-			throw new UnknownStateError(to)
-		if (!this.charInAlphabet(a))
-			throw new UnknownCharError(a)
-        if (this.stateHasTransition(from, a, to))
-            throw new DuplicateTransitionError({ from, a, to })
-
-		this.states = this.states.map(e => {
-			if (e.name === from)
-				e.transitions = [ ...e.transitions, new Transition(from, a, to) ]
-			return e
-		})
-	}
 	removeTransition (from, a, to) {
 		this.states = this.states.map(e => {
 			if (e.name === from)
@@ -50,6 +31,10 @@ export default class NFA extends Automaton {
 			return e
 		})
 	}
+	extraTransitionValidations(from, a, to) {
+        if (this.stateHasTransition(from, a, to))
+            throw new DuplicateTransitionError({ from, a, to })
+	}
 	stateHasTransition(from, a, to) {
 		return this.getState(from).transitions.filter(t => t.a === a && t.to === to)[0]
 	}
@@ -57,7 +42,7 @@ export default class NFA extends Automaton {
 		if (!this.initialState)
 			throw new NoInitialStateError()
 
-		let currStates = this.getStartStates()
+		let currStates = this.getRunStartStates()
 		
 		for (let a of w) {
 			const transitions = this.getTransitionsFor(currStates, a)
@@ -70,7 +55,10 @@ export default class NFA extends Automaton {
 
 		return this.hasFinalState(currStates)
 	}
-	getStartStates() {
+	getConversionStartStates() {
+		return [ this.getState(this.initialState) ]
+    }
+	getRunStartStates() {
 		return [ this.getState(this.initialState) ]
 	}
 	getTransitionsFor(states, a) {
@@ -83,24 +71,22 @@ export default class NFA extends Automaton {
 
 		const dfa = new DFA(this.alphabet)
 
-		let currStates = [ this.getState(this.initialState) ]
+		let currStates = this.getConversionStartStates()
 
 		const stateName = this.joinStateNames(currStates)
 
-		dfa.addState(stateName, this.stateIsFinal(this.initialState))
+		dfa.addState(stateName, this.hasFinalState(currStates))
 		dfa.setInitialState(stateName)
 
 		return this.todfa(dfa, currStates)
 	}
 	todfa(dfa, currStates) {
 		for (const a of this.alphabet) {
-			const transitions = currStates.reduce((accum, curr) => {
-				return [ ...accum, ...curr.transitions.filter(t => t.a === a) ]
-			}, [])
+			const transitions = this.getTransitionsFor(currStates, a)
 
 			if (!transitions.length) continue
 
-			const toStates = transitions.map(t => this.getState(t.to))
+			const toStates = [ ...new Set(transitions.map(t => this.getState(t.to))) ]
 
 			const stateName = this.joinStateNames(toStates)
 
