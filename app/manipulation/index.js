457efb2eea59409c7c72621c4c66241e8dae3204
wupa9 <wupaz9@gmail.com>
2017-02-06 20:23:12 -0600

    Check epsilon-toggle value every time nfa manipulation is selected or reset

--- app/manipulation/index.js
+++ app/manipulation/index.js
@@ -1,14 +1,16 @@
 import DFAManipulation from './DFAManipulation'
 import NFAManipulation from './NFAManipulation'
-import DFA from '../automata/dfa'
-import NFA from '../automata/nfa'
+import DFA  from '../automata/dfa'
+import NFA  from '../automata/nfa'
+import NFAe from '../automata/nfa-e'
 
 export function getCurrentManipulation (currentVisualizer, alphabet, data) {
     switch (currentVisualizer) {
         case 'DFA':
-        return new DFAManipulation(new DFA(alphabet), data)
+            return new DFAManipulation(new DFA(alphabet), data)
         case 'NFA':
-        return new NFAManipulation(new NFA(alphabet), data)
+            const checked = $('#epsilon-toggle').is(':checked')
+            return new NFAManipulation(checked ? new NFAe(alphabet) : new NFA(alphabet), data)
     }
 
     return false
