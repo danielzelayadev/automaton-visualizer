53e35a9677dea9fffee9a4647334f0a5d8751b42
wupa9 <wupaz9@gmail.com>
2017-02-20 16:40:30 -0600

    Refactor evaluate code into different functions

--- app/core/expression-eval.js
+++ app/core/expression-eval.js
@@ -9,54 +9,81 @@ export default function evaluateExpression (exp) {
 }
 
 function evaluate (exp) {
-    let aut = new NFAe([])
-   
-    if (exp.name === 'pipe') {
-        const left  = evaluate(exp.left)
-        const right = evaluate(exp.right)
+    if (exp.name === 'pipe')
+        return evalPipe(exp)
+    if (exp.name === 'concat')
+        return evalConcant(exp)
+    if (exp.name === 'kleene')
+        return evalKleene(exp)
+    if (exp.name === 'character')
+        return evalChar(exp)
+}
 
-        aut.alphabet = [ ...new Set([ ...left.alphabet, ...right.alphabet ]) ]
+function evalPipe(exp) {
+    const aut = new NFAe([])
 
-        aut.addState(`q${ctr++}`, false)
-        aut.states = [ aut.states[0], ...left.states, ...right.states ]
-        aut.addState(`q${ctr++}`)
-        
-        aut.addTransition(aut.states[0].name, epsilon, left.states[0].name)
-        aut.addTransition(aut.states[0].name, epsilon, right.states[0].name)
-        aut.addTransition(left.states[left.states.length - 1].name, epsilon, 
-                          aut.states[aut.states.length - 1].name)
-        aut.addTransition(right.states[right.states.length - 1].name, epsilon, 
-                          aut.states[aut.states.length - 1].name)
-    } else if (exp.name === 'concat') {
-        const left  = evaluate(exp.left)
-        const right = evaluate(exp.right)
+    const left  = evaluate(exp.left)
+    const right = evaluate(exp.right)
 
-        aut.alphabet = [ ...new Set([ ...left.alphabet, ...right.alphabet ]) ]
+    aut.alphabet = [ ...new Set([ ...left.alphabet, ...right.alphabet ]) ]
 
-        aut.states = [  ...left.states, ...right.states ]
-        
-        aut.addTransition(left.states[left.states.length - 1].name, epsilon, right.states[0].name)
-    } else if (exp.name === 'kleene') {
-        const expression = evaluate(exp.expression)
+    aut.addState(`q${ctr++}`, false)
+    aut.states = [ aut.states[0], ...left.states, ...right.states ]
+    aut.addState(`q${ctr++}`)
+    
+    aut.addTransition(aut.states[0].name, epsilon, left.states[0].name)
+    aut.addTransition(aut.states[0].name, epsilon, right.states[0].name)
+    aut.addTransition(left.states[left.states.length - 1].name, epsilon, 
+                        aut.states[aut.states.length - 1].name)
+    aut.addTransition(right.states[right.states.length - 1].name, epsilon, 
+                        aut.states[aut.states.length - 1].name)
 
-        aut.alphabet = [ ...new Set([ ...expression.alphabet ]) ]
+    return aut
+}
 
-        aut.addState(`q${ctr++}`, false)
-        aut.states = [ aut.states[0], ...expression.states ]
-        aut.addState(`q${ctr++}`)
-        
-        aut.addTransition(aut.states[0].name, epsilon, expression.states[0].name)
-        aut.addTransition(aut.states[0].name, epsilon, aut.states[aut.states.length - 1].name)
-        aut.addTransition(expression.states[expression.states.length - 1].name, epsilon, 
-                          aut.states[aut.states.length - 1].name)
-        aut.addTransition(expression.states[expression.states.length - 1].name, epsilon, 
-                          expression.states[0].name)
-    } else if (exp.name === 'character') {
-        aut.addToAlphabet(exp.value)
-        aut.addState(`q${ctr++}`, false)
-        aut.addState(`q${ctr++}`, false)
-        aut.addTransition(aut.states[0].name, exp.value, aut.states[1].name)
-    }
+function evalConcant(exp) {
+    const aut = new NFAe([])
+
+    const left  = evaluate(exp.left)
+    const right = evaluate(exp.right)
+
+    aut.alphabet = [ ...new Set([ ...left.alphabet, ...right.alphabet ]) ]
+
+    aut.states = [  ...left.states, ...right.states ]
+    
+    aut.addTransition(left.states[left.states.length - 1].name, epsilon, right.states[0].name)
+
+    return aut
+}
+
+function evalKleene(exp) {
+    const aut = new NFAe([])
+
+    const expression = evaluate(exp.expression)
+
+    aut.alphabet = [ ...new Set([ ...expression.alphabet ]) ]
+
+    aut.addState(`q${ctr++}`, false)
+    aut.states = [ aut.states[0], ...expression.states ]
+    aut.addState(`q${ctr++}`)
+    
+    aut.addTransition(aut.states[0].name, epsilon, expression.states[0].name)
+    aut.addTransition(aut.states[0].name, epsilon, aut.states[aut.states.length - 1].name)
+    aut.addTransition(expression.states[expression.states.length - 1].name, epsilon, 
+                        aut.states[aut.states.length - 1].name)
+    aut.addTransition(expression.states[expression.states.length - 1].name, epsilon, 
+                        expression.states[0].name)
+
+    return aut
+}
+
+function evalChar(exp) {
+    const aut = new NFAe([])
+
+    aut.addToAlphabet(exp.value)
+    aut.addState(`q${ctr++}`, false)
+    aut.addState(`q${ctr++}`, false)
+    aut.addTransition(aut.states[0].name, exp.value, aut.states[1].name)
 
     return aut
 }
