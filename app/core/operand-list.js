bf8b9c2c2f84278b3be75f97fff8791a20340db2
wupa9 <wupaz9@gmail.com>
2017-02-22 18:18:15 -0600

    Implement operand list

---
+++ app/core/operand-list.js
@@ -0,0 +1,40 @@
+export default class OperandList {
+    selector = ''
+    removePrefix = 'operandRemove'
+    operands = []
+    ctr = 0
+    constructor(selector) { 
+        this.selector = selector 
+        this.clear()
+    }
+    addOperand(name, automaton) {
+        this.operands = [ ...this.operands, { id: this.ctr, name, automaton } ]
+
+        $(this.selector).append(`
+        <li id="${this.ctr}" class="collection-item">
+            <div>
+                ${name}
+                <a href="#" class="secondary-content" id="${this.removePrefix}${this.ctr}">
+                    <i class="material-icons">delete</i>
+                </a>
+            </div>
+        </li>`)
+
+        $(`#${this.removePrefix}${this.ctr++}`)
+            .off('click')
+            .click(this.removeOperand.bind(this))
+    }
+    removeOperand(e) {
+        const li = e.currentTarget.closest('li')
+        const id = li.id
+
+        this.operands = this.operands.filter(o => o.id !== id)
+
+        li.remove()
+    }
+    clear() {
+        $(`${this.selector} .collection-item`).remove()
+        this.operands = []
+        this.ctr = 0
+    }
+}
